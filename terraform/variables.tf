6412b7faf2ed7c4ba9287a7b3c1da5f8b73655b2
Mauricio Almanza <ma@iMacs-MA.local>
2020-04-13 18:02:51 -0500

    Add variable.tf file

---
+++ terraform/variables.tf
@@ -0,0 +1,40 @@
+
+# General variables
+variable "project_name" {
+}
+
+variable "region_name" {
+  description = "The region that this terraform configuration will instanciate at."
+  default     = "us-central1"
+}
+
+variable "zone_name" {
+  description = "The zone that this terraform configuration will instanciate at."
+  default     = "us-central1-a"
+}
+
+
+
+# Varibles for VM Instances
+variable "machine_type" {
+  description = "The size that this instance will be."
+  default     = "f1-micro"
+}
+
+variable "image_name" {
+  description = "The kind of VM this instance will become"
+  default     = "cos-cloud/cos-stable"
+}
+
+# Varible for network
+variable "docker_tag" {
+  description = "Network tag used by VM instances"
+  default     = "docker-tag"
+}
+
+variable "gcp_credentials" {}
+
+variable "bucket_backend" {
+  default     = "cloudma-terraform-backend"
+}
+
